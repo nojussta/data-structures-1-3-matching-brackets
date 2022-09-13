ec469f9ed2dc3cbf7cda448eb8145e7ed3d9dfa6
nojussta <nojus.stasiunas2@gmail.com>
2022-09-13 18:44:54 +0300

    add/boolean-task-fulfilled

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String test1 = "}";
+        String test2 = "{()}{[]}";
+        String test3 = "[{}}";
+        String test4 = "{()[{}]}{}";
+        String test5 = "{(})";
+        String test6 = "([(]{)})";
+
+        System.out.println(TaskUtils.isBracketsIsMatch(test6));
     }
 }
--- src/TaskUtils.java
+++ src/TaskUtils.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class TaskUtils {
+import java.util.Stack;
+
+public class TaskUtils {
+    public static boolean isBracketsIsMatch(String input) {
+
+        Stack<Character> stack = new Stack<Character>();
+
+        char symbol;
+        for (int i = 0; i < input.length(); i++) {
+            symbol = input.charAt(i);
+            if (symbol == '(')
+                stack.push(symbol);
+            else if (symbol == '{')
+                stack.push(symbol);
+            else if (symbol == ')')
+                if (stack.empty())
+                    return false;
+                else if (stack.peek() == '(')
+                    stack.pop();
+                else
+                    return false;
+            else if (symbol == '}')
+                if (stack.empty())
+                    return false;
+                else if (stack.peek() == '{')
+                    stack.pop();
+                else
+                    return false;
+        }
+        return stack.empty();
+    }
 }
